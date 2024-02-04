6a5b919d83af3e7a2aa6ef8fc6ef606e57a1f77c
Derrick-Edowen <derrickedowen@hotmail>
2024-02-04 02:43:58 -0500

    nice updates  and  light box

--- src/Components/ForRent/ForRent.js
+++ src/Components/ForRent/ForRent.js
@@ -5,36 +5,37 @@ import { APIProvider, Map, Marker } from '@vis.gl/react-google-maps';
 import Contact from '../Contact/Contact';
 import './Indexr.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import placeHouse from '../../Assets/Images/placeHouse.jpg'
-import image1 from '../../Assets/Images/living1.jpg'                                                                            
-import image2 from '../../Assets/Images/backyard1.jpg'
-import image3 from '../../Assets/Images/yard1.jpg'
-import image4 from '../../Assets/Images/house1.jpg'
-import image5 from '../../Assets/Images/kitchen1.jpg'
-import noImg from '../../Assets/Images/noimg.jpg'
+import noImg from '../../Assets/Images/noimg.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-import { faBed } from '@fortawesome/free-solid-svg-icons';
-import { faClock } from '@fortawesome/free-solid-svg-icons';
-import { faBath } from '@fortawesome/free-solid-svg-icons';
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
-import { faHouseUser } from '@fortawesome/free-solid-svg-icons';
+import { faMagnifyingGlass, faChevronLeft, faChevronRight, faBed,
+ faClock, faBath, faCircleXmark, faHouseUser,  faFire,  faWind,
+faSquareParking, faJugDetergent} from '@fortawesome/free-solid-svg-icons';
+
 
 function ForRent() {
 
-//Mapping
 const [apiData, setApiData] = useState([]);
 const [infoData, setInfoData] = useState([]);
 const [position, setPosition] = useState({ lat: 43.6426, lng: -79.3871 });
 const [cardIndex, setCardIndex] = useState(0);
-const [searchClicked, setSearchClicked] = useState(false); // Track if search is clicked
+const [searchClicked, setSearchClicked] = useState(false); 
 const [isLoading, setIsLoading] = useState(false);
 const [selectedCard, setSelectedCard] = useState(null);
-const [isExpanded, setIsExpanded] = useState(false);
-const [zoomLevel, setZoomLevel] = useState(10); // Set an initial zoom level
+const [imageUrls, setImageUrls] = useState([]);
+const [lightboxActive, setLightboxActive] = useState(false);
+const [selectedCardIndex, setSelectedCardIndex] = useState(null);
+const [zoomLevel, setZoomLevel] = useState(11); 
+const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
+const handleNextImage = () => {
+  const nextIndex = (currentImageIndex + 1) % imageUrls[selectedCardIndex].images.length;
+  setCurrentImageIndex(nextIndex);
+};
 
+const handlePrevImage = () => {
+  const prevIndex = (currentImageIndex - 1 + imageUrls[selectedCardIndex].images.length) % imageUrls[selectedCardIndex].images.length;
+  setCurrentImageIndex(prevIndex);
+};
 const updateMapLocation = async (address) => {
   const apiKey = 'AIzaSyCMPVqY9jf-nxg8fV4_l3w5lNpgf2nmBFM'; // Replace with your Google Maps API key
   const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${apiKey}`;
@@ -50,7 +51,7 @@ const updateMapLocation = async (address) => {
     if (results && results.length > 0) {
       const { lat, lng } = results[0].geometry.location;
       setPosition({ lat, lng });
-      setZoomLevel(15); // Set your desired zoom level here
+      setZoomLevel(16); // Set your desired zoom level here
     } else {
       window.alert('Location Not Found: Try Using More Descriptive Words!');
     }
@@ -88,7 +89,7 @@ const updateMapLocation = async (address) => {
   const apiKey = 'AIzaSyCMPVqY9jf-nxg8fV4_l3w5lNpgf2nmBFM';
   const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${apiKey}`;
   try {
-    setIsLoading(true); // Set loading state to true during data fetching
+    setIsLoading(true); 
 
     const response = await fetch(geocodeUrl);
     if (!response.ok) {
@@ -122,32 +123,59 @@ const updateMapLocation = async (address) => {
           setApiData(estateResponse.data); 
 
           const zpidList = estateResponse.data.props.map((item) => item.zpid);
-      const maxRequestsPerSecond = 2; // Define the maximum requests per second
-      const delayBetweenRequests = 1000 / maxRequestsPerSecond; // Calculate the delay between requests
+  const maxRequestsPerSecond = 2;
+  const delayBetweenRequests = 1000 / maxRequestsPerSecond;
 
-      const infoDataArray = [];
-      for (let i = 0; i < zpidList.length; i++) {
-        const zpid = zpidList[i];
-        const response = await axios.get('https://zillow-com1.p.rapidapi.com/property', {
-          params: { zpid },
-          headers: {
-            'X-RapidAPI-Key': 'f2d3bb909amsh6900a426a40eabep10efc1jsn24e7f3d354d7',
-            'X-RapidAPI-Host': 'zillow-com1.p.rapidapi.com'
-          }
-        });
-        infoDataArray.push(response.data);
-        if (i < zpidList.length - 1) {
-          await new Promise((resolve) => setTimeout(resolve, delayBetweenRequests));
-        }
-      }
+  const infoDataArray = [];
+const imageUrlsArray = [];
 
-      setInfoData(infoDataArray);
-      setIsLoading(false); // Set loading state to false after data is fetched
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      setIsLoading(false); // Ensure loading state is set to false in case of error
+const fetchPropertyData = async (zpid) => {
+  const apiKey = 'f2d3bb909amsh6900a426a40eabep10efc1jsn24e7f3d354d7';
+  const propertyUrl = 'https://zillow-com1.p.rapidapi.com/property';
+  const imagesUrl = 'https://zillow-com1.p.rapidapi.com/images';
+
+  const propertyResponse = await axios.get(propertyUrl, {
+    params: { zpid },
+    headers: {
+      'X-RapidAPI-Key': apiKey,
+      'X-RapidAPI-Host': 'zillow-com1.p.rapidapi.com'
     }
-  };
+  });
+
+  // Introduce a delay before making the second request
+  await new Promise(resolve => setTimeout(resolve, delayBetweenRequests));
+
+  const imageResponse = await axios.get(imagesUrl, {
+    params: { zpid },
+    headers: {
+      'X-RapidAPI-Key': apiKey,
+      'X-RapidAPI-Host': 'zillow-com1.p.rapidapi.com'
+    }
+  });
+  return { property: propertyResponse.data, images: imageResponse.data };
+};
+
+for (let i = 0; i < zpidList.length; i++) {
+  const zpid = zpidList[i];
+  const { property, images } = await fetchPropertyData(zpid);
+
+  // Use property and images data as needed
+  infoDataArray.push({ ...property, images });
+  imageUrlsArray.push(images);
+
+  if (i < zpidList.length - 1) {
+    await new Promise((resolve) => setTimeout(resolve, delayBetweenRequests));
+  }
+}
+
+setInfoData(infoDataArray);
+setImageUrls(imageUrlsArray);
+  setIsLoading(false);
+} catch (error) {
+  console.error('Error fetching data:', error);
+  setIsLoading(false);
+}
+};
 
       function adjustTextSize() {
         const cards = document.querySelectorAll('.card');
@@ -165,6 +193,22 @@ const updateMapLocation = async (address) => {
       useEffect(() => {
         adjustTextSize();
       }, [infoData, cardIndex]);
+
+
+      const handleOpenLightbox = (index) => {
+        setSelectedCardIndex(index);
+        setLightboxActive(true);
+        (async () => {
+      // Update map location when a card is clicked
+      const selectedProperty = apiData.props[index];
+      await updateMapLocation(selectedProperty.address);
+    })();
+      };
+    
+      const handleCloseLightbox = () => {
+        setSelectedCardIndex(null);
+        setLightboxActive(false);
+      };
     return (
       
         <div className='lists'>
@@ -176,60 +220,81 @@ const updateMapLocation = async (address) => {
       </div>
     ) : (
       <>
-        {apiData.props && apiData.props.length > 0 && (
-          <div className={`cardContainer ${selectedCard ? 'hidden' : ''}`}>
-            {searchClicked && infoData && infoData.length > 0 && (
-              apiData.props.map((property, index) => (
-                <div className={`cardi ${selectedCard === index ? 'expanded' : ''}`}
+      {apiData.props && apiData.props.length > 0 && (
+        <div className="cardContainer">
+          {searchClicked && infoData && infoData.length > 0 && (
+            apiData.props.map((property, index) => (
+              <div
+                className="cardi"
                 key={index}
-                onClick={() => handleCardClick(index)}>
-                  <img src={property.imgSrc || noImg}
-                    alt={'No Image Available'}/>
-                  <div className="cardText">
-                      <div className='pPrice'>${property.price}/Month<br /></div>
-                      <div className='pAddress'>{property.address}</div>
-                    <p>
-                      <FontAwesomeIcon icon={faBed} size="lg" style={{ color: "#1d1e20" }} />&nbsp; {property.bedrooms}&nbsp;&nbsp;&nbsp;&nbsp;
-                      <FontAwesomeIcon icon={faBath} size="lg" style={{ color: "#1d1e20" }} />&nbsp; {property.bathrooms}&nbsp;&nbsp;&nbsp;&nbsp;
-                      <FontAwesomeIcon icon={faClock} size="xs" />&nbsp;{infoData[index]?.timeOnZillow || "Unknown"}<br />
-                    </p>
-                    
-              
-                  </div>
-                  {selectedCard && (
-        <div className="expandedCardContainer">
-          {/* Display detailed information from the selected card */}
-          <div className="expandedCard">
-          <img src={property.imgSrc || noImg}
-                    alt={'No Image Available'}/>
-          <div className="cardText">
-          <p className='pPrice'>${property.price}/Month</p>
-            <p className='pAddress'>{property.address}</p>
-            <p>
-            <FontAwesomeIcon icon={faBed} size="lg" style={{ color: "#1d1e20" }} />&nbsp; {property.bedrooms}&nbsp;&nbsp;&nbsp;&nbsp;
-            <FontAwesomeIcon icon={faBath} size="lg" style={{ color: "#1d1e20" }} />&nbsp; {property.bathrooms}&nbsp;&nbsp;&nbsp;&nbsp;
-            <FontAwesomeIcon icon={faClock} size="xs" />&nbsp;{infoData[index]?.timeOnZillow || "Unknown"}<br />
-            </p>
-            </div>
-            {infoData[index]?.description}<br />
-                  Parking Status: {infoData[index]?.resoFacts.parkingCapacity} parking space(s)<br />
-                  Heating: {infoData[index]?.resoFacts.heating[0]}/{infoData[index]?.resoFacts.heating[1]}<br />
-                  Cooling: {infoData[index]?.resoFacts.cooling[0]}<br />
-                  MLS#: {infoData[index]?.mlsid}<br />
-                  BROKERAGE: {infoData[index]?.brokerageName}<br /><br />
-          </div>
+                onClick={() => handleOpenLightbox(index)}
+              >
+                <img src={property.imgSrc || noImg} alt={'Not Available'} />
+                <div className="cardText">
+                  <div>${property.price}/Month<br /></div>
+                  <div>{property.address}</div>
+                  <p>
+                  
+                  <FontAwesomeIcon icon={faBed} size="xl" style={{color: "#190000",}} />&nbsp;{property.bedrooms}&nbsp;&nbsp;&nbsp;&nbsp;
+                  <FontAwesomeIcon icon={faBath} size="xl" style={{color: "#190000",}}/>&nbsp;{property.bathrooms}&nbsp;&nbsp;&nbsp;&nbsp;
+                  <FontAwesomeIcon icon={faClock} size="lg" style={{color: "#190000",}}  />&nbsp;{infoData[index]?.timeOnZillow || "Unknown"}<br />
+                </p>  
+                </div>
+              </div>
+            ))
+          )}
         </div>
       )}
-          
-                </div>
-                
-              ))
-            )}
+
+{lightboxActive && selectedCardIndex !== null && (
+  <div className="lightbox" onClick={handleCloseLightbox}>
+    <div className="lightbox-content" onClick={(e) => e.stopPropagation()}>
+      {/* Lightbox content goes here */}
+      {infoData[selectedCardIndex] && imageUrls[selectedCardIndex] && (
+        <>
+        <div className='side-by-side-container'>
+        <button className="lightbox-left" onClick={handlePrevImage}>
+            <FontAwesomeIcon icon={faChevronLeft} size="lg" />
+          </button>
+        <img src={imageUrls[selectedCardIndex].images[currentImageIndex] || noImg} alt="Sorry, Not Available :(" />
+          <button className="lightbox-right" onClick={handleNextImage}>
+            <FontAwesomeIcon icon={faChevronRight} size="lg" />
+          </button> 
+              
+          <div className="cardText">
+          <div className='pAddress'>{apiData.props[selectedCardIndex].address}</div>
+          <div className='pPrice'>${apiData.props[selectedCardIndex].price}/Month<br /></div>
+        <p>
+        <FontAwesomeIcon icon={faBed} size="xl" style={{color: "#492903",}} />&nbsp; {apiData.props[selectedCardIndex].bedrooms}&nbsp;&nbsp;&nbsp;&nbsp;
+        <FontAwesomeIcon icon={faBath} size="xl" style={{color: "#492903",}}/>&nbsp; {apiData.props[selectedCardIndex].bathrooms}&nbsp;&nbsp;&nbsp;&nbsp;
+        <FontAwesomeIcon icon={faClock} size="lg" style={{color: "#3d0000",}} />&nbsp; {infoData[selectedCardIndex]?.timeOnZillow || "Unknown"}             
+        </p>
+            {infoData[selectedCardIndex]?.description}<br />
+            <FontAwesomeIcon icon={faSquareParking} size="lg" style={{color: "#065b0b",}} /> - {infoData[selectedCardIndex]?.resoFacts.parkingCapacity} parking space(s)<br />
+            <FontAwesomeIcon icon={faFire} size="lg" style={{color: "#bf0d0d",}} /> - {infoData[selectedCardIndex]?.resoFacts.heating[0]}/{infoData[selectedCardIndex]?.resoFacts.heating[1]}<br />
+            <FontAwesomeIcon icon={faWind} size="lg" style={{color: "#006bbd",}} /> - {infoData[selectedCardIndex]?.resoFacts.cooling[0]}<br />
+            <FontAwesomeIcon icon={faJugDetergent} size="lg" style={{ color: "#012665" }}/> - {infoData[selectedCardIndex]?.resoFacts.laundryFeatures &&infoData[selectedCardIndex]?.resoFacts.laundryFeatures.length > 0? infoData[selectedCardIndex]?.resoFacts.laundryFeatures[0]: "Unknown Laundry Status"}<br />
+                  MLS#: {infoData[selectedCardIndex]?.mlsid}<br />
+                  Listing Brokerage: {infoData[selectedCardIndex]?.brokerageName}<br />        
           </div>
+          </div>
+          <div className='map'>
+<APIProvider apiKey='AIzaSyCMPVqY9jf-nxg8fV4_l3w5lNpgf2nmBFM'>
+  <Map center={position} zoom={zoomLevel} mapTypeId ='hybrid' >
+    <Marker position={position}/>
+  </Map>
+</APIProvider>
+        </div>
           
-        )}
-        
-      </>
+        <button className="lightbox-close" onClick={handleCloseLightbox}>
+              <FontAwesomeIcon icon={faCircleXmark} size="xl" />
+            </button>         
+        </>
+      )}
+    </div>
+  </div>
+)}
+    </>
     )}
     </main>
         <aside  className='halves'>
@@ -274,13 +339,7 @@ const updateMapLocation = async (address) => {
           </select>
           <button className='searchBtn' onClick={handleSearch}>Search&nbsp;&nbsp;<FontAwesomeIcon icon={faMagnifyingGlass} style={{color: "#fafafa",}} /></button>
         </div>
-        <div className='map'>
-<APIProvider apiKey='AIzaSyCMPVqY9jf-nxg8fV4_l3w5lNpgf2nmBFM'>
-  <Map center={position} zoom={zoomLevel}>
-    <Marker position={position}/>
-  </Map>
-</APIProvider>
-        </div>
+        
         </aside>
         
         
@@ -373,4 +432,12 @@ export default ForRent;
                   Cooling: {infoData[index]?.resoFacts.cooling[0]}<br />
                   MLS#: {infoData[index]?.mlsid}<br />
                   BROKERAGE: {infoData[index]?.brokerageName}<br /><br />
+
+                  <div className='map'>
+<APIProvider apiKey='AIzaSyCMPVqY9jf-nxg8fV4_l3w5lNpgf2nmBFM'>
+  <Map center={position} zoom={zoomLevel}>
+    <Marker position={position}/>
+  </Map>
+</APIProvider>
+        </div>
       */
