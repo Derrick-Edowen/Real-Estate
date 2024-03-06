bcf515726ad9fd4019dac43a898751028d8ec6eb
Derrick-Edowen <derrickedowen@hotmail>
2024-03-06 10:41:19 -0500

    trying

--- server.js
+++ server.js
@@ -25,11 +25,11 @@ pool = mysql.createPool({
 });
 
 // API endpoints
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 // Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 // Login endpoint
 app.post('/login', async (req, res) => {
